a93a6becd604f6ac010fa5e29e9fcf93e7b6b11f
taufiqur0991 <taufiqur0991@gmail.com>
2025-11-15 17:39:31 +0700

    update package @fastify/jwt

--- src/plugins/jwt.js
+++ src/plugins/jwt.js
@@ -1,7 +1,7 @@
 import fp from "fastify-plugin";
 
 export default fp(async (fastify) => {
-  fastify.register(import("fastify-jwt"), {
+  fastify.register(import("@fastify/jwt"), {
     secret: process.env.JWT_SECRET,
   });
 
